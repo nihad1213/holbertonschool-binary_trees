33c4a8a1f421fbd5cc811d17a6493726223ac4e5
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-08 14:56:01 +0400

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-        int depth = 0;
+        unsigned int depth = 0;
         binary_tree_t *currentNode;
 
         currentNode = tree->parent;
