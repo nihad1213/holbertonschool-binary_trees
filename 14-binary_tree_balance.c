82fe57b423a6964dd50193f0ce4e4f3ba77cd202
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-10 00:13:07 +0400

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((left > right) ? l : right);
+		return ((left > right) ? left : right);
 	}
 	return (0);
 }
